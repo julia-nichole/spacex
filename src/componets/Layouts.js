c3cc7bde094c0e88e0a57996602381c771bfe95c
julia-nichole <juliagalan191994@icloud.com>
2020-10-29 17:51:22 -0500

    sticky footer

--- src/componets/Layouts.js
+++ src/componets/Layouts.js
@@ -1,14 +1,23 @@
 import React from 'react';
+import styled from 'styled-components';
 /* Layout Components */
 import Header from './Header';
 import Footer from './Footer';
+const StyledLayout = styled.div`
+    min-height: 100vh;
+    display: flex;
+    flex-direction: column;
+    main {
+        flex-grow: 1;
+    }
+`;
 export default function Layout(props) {
     return (
-        <div>
+        <StyledLayout>
             <Header />
                 <main>
                 </main>
             <Footer />
-        </div>
+        </StyledLayout>
     );
 };
