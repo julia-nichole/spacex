69ed548ba338697a3e83e839ced46698f8f2582c
julia-nichole <juliagalan191994@icloud.com>
2020-10-29 18:16:24 -0500

    client side routing

--- src/componets/Layouts.js
+++ src/componets/Layouts.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import styled from 'styled-components';
+import { Switch,Route,Redirect} from 'react-router-dom';
 /* Layout Components */
 import Header from './Header';
 import Footer from './Footer';
+/* Page Level Components */
+import Home from '../pages/home';
+import Login from  '../pages/login';
+import Launches from '../pages/launches';
+
+
 const StyledLayout = styled.div`
     min-height: 100vh;
     display: flex;
@@ -16,6 +23,11 @@ export default function Layout(props) {
         <StyledLayout>
             <Header />
                 <main>
+                <Switch>
+                        <Route exact path="/" render={props => 
+                            <Home />
+                        } />
+                    </Switch>
                 </main>
             <Footer />
         </StyledLayout>
